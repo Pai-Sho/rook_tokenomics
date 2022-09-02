d48c4c801ed3d3fbd974cfe2456ae7af142bcc56
Pai-Sho <paisho@keeperdao.com>
2022-09-02 13:23:45 -0600

    Cleanup

--- Home.py
+++ Home.py
@@ -2,11 +2,9 @@ import streamlit as st
 
 from PIL import Image
 
-icon = Image.open('images/KeeperDAO_Logo_Icon_White.png')
-st.set_page_config(
-    page_title="ROOK Tokenomics",
-    page_icon=icon
-)
+icon = Image.open("images/KeeperDAO_Logo_Icon_White.png")
+st.set_page_config(page_title="ROOK Tokenomics", page_icon=icon)
 
-with open('markdown/Home.md') as homepage:
-    st.markdown(homepage.read())
+st.markdown(
+    "### **NOTE:** This model only attempts to simulate the things we have some control over, with somewhat reasonable methods. It is not an accurate representation of reality, and should only be used to compare the _relative_ effects of changing these parameters in a vacuum."
+)
